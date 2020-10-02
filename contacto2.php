cebacd57a0804af8a05b37346eb91b9845924fa3
Blascha <71779341+Blascha@users.noreply.github.com>
2020-10-02 11:57:13 -0300

    Add files via upload

---
+++ contacto2.php
@@ -0,0 +1,12 @@
+    <?php
+$nombre = $_POST["nombre"];
+$email = $_POST["email"];
+$mensaje = $_POST["mensaje"];
+$queRecibeMRS = "Nombre: " . $nombre . ". mail: " . $email. " mensaje: " . $mensaje . ".";
+$emailTo = 'info@mrsconstrucciones.com.ar';
+$emailSubject = 'Desde Pagina WEB';
+$headers = 'Desde Pagina WEB'
+@mail($emailTo, $emailSubject, $queRecibeMRS);
+header('Location: prueba.html');
+exit;
+?>
